5f6d282407e1ee755b43e8652f7490d66b0baf03
Ryan Moore <kiwi.ryanmoore@gmail.com>
2020-05-09 11:29:49 +1200

    bugfixing

--- objutils/src/main/java/com/spurposting/plugin/SBUtils.java
+++ objutils/src/main/java/com/spurposting/plugin/SBUtils.java
@@ -14,6 +14,9 @@ import org.bukkit.scoreboard.*;
 
 public class SBUtils implements CommandExecutor {
 
+    int TICKRATE = 20;
+
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         
@@ -33,25 +36,46 @@ public class SBUtils implements CommandExecutor {
             for(String entry : entries) {
                 Set<Score> scores = board.getScores(entry);
                 for(Score score : scores) {
-                    if (score.getObjective​().equals(obj) && score.getScore() > 0) {
-                        printLns.add(String.format("%04d", score.getScore()) + " " + entry);
+                    if (score.getObjective().equals(obj) && score.getScore() > 0) {
+                        int time = score.getScore();
+                        int timeSeconds = (int)Math.floor(time / TICKRATE);
+                        int timeMinutes = (int)Math.floor(timeSeconds / 60);
+                        int ticks = (time % 20) / 2;
+                        int seconds = timeSeconds % 60;
+                        printLns.add(
+                            String.format("%02d", timeMinutes)
+                            + ":" + String.format("%02d", seconds)
+                            + "." + ticks
+                            + " " + entry);
                     }
                 }
             }
-            
+
             Collections.sort(printLns);
-            
+
             int i = 0;
             int j = printLns.size();
-            System.out.println(j);
-            
+
             if (args.length == 4) {
-                i = Integer.parseInt(args[2]);
-                j = Integer.parseInt(args[3]);
+                if (Integer.parseInt(args[2]) >= printLns.size()) {
+                    sender.sendMessage("No Results Yet");
+                    return true;
+                } else {
+                    i = Integer.parseInt(args[2]);
+                }
+                if (Integer.parseInt(args[3]) > printLns.size()) {
+                    j = printLns.size();
+                } else {
+                    j = Integer.parseInt(args[3]);
+                }
             }
-            
-            while(i < j) {
-                sender.sendMessage(printLns.get(i));
+
+            while (i < j) {
+                if (i < 9) {
+                    sender.sendMessage("  " + (i + 1) + ". " + printLns.get(i));
+                } else {
+                    sender.sendMessage((i + 1) + ". " + printLns.get(i));
+                }
                 i++;
             }
         }
@@ -59,18 +83,54 @@ public class SBUtils implements CommandExecutor {
             if (args.length != 2) {
                 sender.sendMessage("Incorrectly formatted command");
             }
-            
-            for(String entry : entries) {
+
+            for (String entry : entries) {
                 Set<Score> scores = board.getScores(entry);
-                for(Score score : scores) {
-                    if (score.getObjective​().equals(obj) && score.getScore() > 0) {
+                for (Score score : scores) {
+                    if (score.getObjective().equals(obj) && score.getScore() > 0) {
                         board.resetScores(entry);
                     }
                 }
             }
             sender.sendMessage(String.format(
                 "Cleared Objective %s (display name %s)",
-                obj.getName​(), obj.getDisplayName​()));
+                obj.getName(), obj.getDisplayName()));
+        }
+        if (args[0].equals("double")) {
+            if (args.length != 2) {
+                sender.sendMessage("Incorrectly formatted command");
+            }
+
+            for (String entry : entries) {
+                Set<Score> scores = board.getScores(entry);
+                for (Score score : scores) {
+                    if (score.getObjective().equals(obj) && score.getScore() > 0) {
+                        score.setScore(score.getScore() * 2);
+                    }
+                }
+            }
+            sender.sendMessage(String.format(
+                "Doubled Objective %s (display name %s)",
+                obj.getName(), obj.getDisplayName()));
+        }
+        if (args[0].equals("copy")) {
+            if (args.length != 3) {
+                sender.sendMessage("Incorrectly formatted command");
+            }
+            Objective newObj = board.getObjective(args[2]);
+            for (String entry : entries) {
+                Set<Score> scores = board.getScores(entry);
+                for (Score score : scores) {
+                    if (score.getObjective().equals(obj) && score.getScore() > 0) {
+                        Score newScore = newObj.getScore(entry);
+                        newScore.setScore(score.getScore());
+                    }
+                }
+            }
+            
+            sender.sendMessage(String.format(
+                "Copied Objective %s to %s",
+                obj.getName(), newObj.getName()));
         }
         return true;
     }
--- objutils/src/main/java/com/spurposting/plugin/ScoreboardFlip.java
+++ objutils/src/main/java/com/spurposting/plugin/ScoreboardFlip.java
@@ -14,9 +14,12 @@ import org.bukkit.scoreboard.*;
 
 public class ScoreboardFlip implements CommandExecutor {
 
+    int TICKRATE = 20;
+
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (args.length != 2) {
+        if (args.length != 2 && args.length != 3) {
             sender.sendMessage("Incorrectly formatted command");
         }
         
@@ -25,6 +28,11 @@ public class ScoreboardFlip implements CommandExecutor {
 
         Objective oldObj = board.getObjective(args[0]);
         Objective newObj = board.getObjective(args[1]);
+        Objective isActive = null;
+        
+        if (args.length == 3) {
+            isActive = board.getObjective(args[2]);
+        }
 
         Set<String> entries = board.getEntries();
 
@@ -35,10 +43,10 @@ public class ScoreboardFlip implements CommandExecutor {
         for(String entry : entries) {
             Set<Score> scores = board.getScores(entry);
             for(Score score : scores) {
-                if (score.getObjective​().equals(oldObj) && score.getScore() > 0) {
+                if (score.getObjective().equals(oldObj) && score.getScore() > 0) {
                     oldEntries.add(entry);
                 }
-                if (score.getObjective​().equals(newObj) && score.getScore() > 0) {
+                if (score.getObjective().equals(newObj) && score.getScore() > 0) {
                     newEntries.add(entry);
                 }
             }
@@ -50,12 +58,19 @@ public class ScoreboardFlip implements CommandExecutor {
             }
             board.resetScores(entry);
         }
-        System.out.println(oldEntries);
-        System.out.println(newEntries);
         if (newObj != null && oldObj != null) {
             outer:
             for(String oldEntry : oldEntries) {
-                String oldEntryFormatted = String.format("%04d", oldObj.getScore(oldEntry).getScore()) + " " + oldEntry;
+                int time = oldObj.getScore(oldEntry).getScore();
+                int timeSeconds = (time / TICKRATE);
+                int timeMinutes = (int)Math.floor(timeSeconds / 60);
+                int seconds = timeSeconds % 60;
+                int ticks = (time % 20) / 2;
+                String oldEntryFormatted = String.format("%02d", timeMinutes)
+                 + ":" + String.format("%02d", seconds)
+                 + "." + ticks
+                 + " " + oldEntry;
+                
                 for (String newEntry : newEntries) {
                     if (newEntry.contains(oldEntry)) {
                         if (newEntry.equals(oldEntryFormatted)) {
@@ -64,12 +79,31 @@ public class ScoreboardFlip implements CommandExecutor {
                         } else {
                             // else
                             // remove old entry
-                            board.resetScores(newEntry);
-                            // add new entry
-                            newEntry = oldEntryFormatted;
-                            Score score = newObj.getScore(newEntry);
-                            score.setScore(1);
-                            sender.sendMessage(String.format("updated score for %s", newEntry));
+                            Set<Score> scores = board.getScores(oldEntry);
+                            for(Score score : scores) {
+                                // if timing is active
+                                if (score.getObjective().equals(isActive) && score.getScore() > 0) {
+                                    board.resetScores(newEntry);
+                                    // add new entry
+                                    newEntry = oldEntryFormatted;
+                                    Score newScore = newObj.getScore(newEntry);
+                                    newScore.setScore(1);
+                                    sender.sendMessage(String.format("updated score for %s", newEntry));
+                                    continue outer;
+                                }
+                            }
+                            
+                            // if timing is not active but new score is less
+                            if (oldEntryFormatted.compareTo(newEntry) < 0) {
+                                board.resetScores(newEntry);
+                                // add new entry
+                                newEntry = oldEntryFormatted;
+                                Score score = newObj.getScore(newEntry);
+                                score.setScore(1);
+                                sender.sendMessage(String.format("updated score for %s", newEntry));
+                            }
+
+                            // else do nothing
                             continue outer;
                         }
                     }
@@ -80,10 +114,10 @@ public class ScoreboardFlip implements CommandExecutor {
             }
             sender.sendMessage(String.format("Flipped %s and wrote to %s", args[0], args[1]));
         } else {
-            if (oldObj != null) {
+            if (oldObj == null) {
                 sender.sendMessage(String.format("Objective %s not found", args[0]));
             }
-            if (newObj != null) {
+            if (newObj == null) {
                 sender.sendMessage(String.format("Objective %s not found", args[1]));
             }
         }
