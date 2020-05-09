64720a2526ac3c44a8a9cd4f501685af5e53d87b
Ryan Moore <kiwi.ryanmoore@gmail.com>
2020-05-10 11:29:24 +1200

    post event commit

--- objutils/src/main/java/com/spurposting/plugin/StartClock.java
+++ objutils/src/main/java/com/spurposting/plugin/StartClock.java
@@ -6,8 +6,13 @@ import org.bukkit.command.*;
 
 import org.bukkit.Bukkit;
 
-import java.time.format.DateTimeFormatter;  
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 
 public class StartClock implements CommandExecutor {
     
@@ -15,9 +20,15 @@ public class StartClock implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         
+        int offset = 0;
+        if (args.length == 4) {
+            offset = Integer.parseInt(args[3]);
+        }
+
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
         ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
-        LocalDateTime now = LocalDateTime.now();
+
+        ZonedDateTime now = ZonedDateTime.now(ZoneOffset.ofHoursMinutes(0, offset));
                 
         if (args[0].equals("set") || args[0].equals("create")) {
             String name = args[1];
@@ -30,8 +41,7 @@ public class StartClock implements CommandExecutor {
         if (args[0].equals("update")) {
             
             String name = args[1];
-            String updateCommand = "hd setline " + name + " 1 " + dtf.format(now);
-            sender.sendMessage(updateCommand);
+            String updateCommand = "hd setline " + name + " " + args[2] + " " + dtf.format(now);
             Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), updateCommand);
         }
 
